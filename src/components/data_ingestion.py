484b2d78170e1f44288520a240cd98db1ba8b82c
rohanp04 <rohanprajapati2504@gmail.com>
2025-01-26 18:15:05 +0530

    Hyper Parameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainer
 from src.components.model_trainer import ModelTrainerConfig
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 @dataclass
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import sys
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(filepath, obj):
@@ -19,26 +20,30 @@ def save_object(filepath, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        for i, model_name in enumerate(models.keys()):
+            model = models[model_name]
+            param = params.get(model_name, {})
 
+            gs = GridSearchCV(model, param, cv=3, n_jobs=-1, scoring='r2')
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) 
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_r2 = r2_score(y_train, y_train_pred)
-
             test_r2 = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_r2
+            report[model_name] = test_r2
         
         return report
 
-    except:
-        pass
+    except Exception as e:
+        print(f"Error in model evaluation: {e}")
+        return None 
